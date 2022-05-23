f1e31f241d3a95f51b90b02239ad4da9ac9fcbea
M.Ebad Arshad <104248177+ebad-arshad@users.noreply.github.com>
2022-05-23 23:07:32 +0500

    completed assignment 1

---
+++ app.js
@@ -0,0 +1,58 @@
+// Chapter 1
+
+// Q1
+// alert("Welcome User");
+
+// Q2
+// alert("Error! Please enter a valid password.");
+
+// Q3
+// alert("Welcome to JS Land...\nHappy Coding!");
+
+// Q4
+// alert("Welcome to JS Land...");
+// alert("Happy Coding!");
+
+// Q5
+// console.log("Hello... I can run JS through my web browser's console");
+
+
+// Chapter 2
+
+
+// Q1
+// var username;
+
+// Q2
+// var myName = "M.Ebad Arshad";
+
+// Q3
+// var message = "Hello World";
+// alert(message);
+
+// Q4
+// var sName = "Ebad Arshad";
+// var sAge = "18 years old";
+// var sQualification = "Certified Mobile Application Development";
+// alert(sName);
+// alert(sAge);
+// alert(sQualification);
+
+// Q5
+// var multiLine = "PIZZA\nPIZZ\nPIZ\nPI\nP";
+// alert(multiLine);
+
+// Q6
+// var email = "ebadarshad69@gmail.com";
+// alert("My email address is " + email);
+
+// Q7
+// var book = "A smarter way to learn JavaScript";
+// alert("I am trying to learn from the Book " + book);
+
+// Q8
+// document.write("Yah! I can write HTML content through JavaScript");
+
+// Q9
+// var design = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+// alert(design);
